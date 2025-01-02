9d01ea19f5edc1c3de0ef8e7aa7934b458fb66cf
Leonardo <macstore27@gmail.com>
2025-01-02 18:03:42 -0500

    agrregamos varias cosas a los proyectos

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,51 @@
-const {src, dest} = require('gulp');
+const {src, dest, watch, series, parallel} = require('gulp');
+// css 
 const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+// imagenes
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+const cssnano = require('cssnano');
 
-
-function css( done ) {
-    src('src/scss/app.scss')
+function css() {
+    return src('src/scss/app.scss')
         .pipe( sass())
+        .pipe( postcss([autoprefixer, cssnano()]))
         .pipe( dest('build/css'))
+}
 
-    done();
+function imagenes() {
+    return src('src/img/**/*')
+            .pipe( imagemin({optimizationLevel: 3}))
+            .pipe( dest('build/img'))
+}
+
+function versionWebp() {
+    return src('src/img/**/*.{png,jpg}')
+            .pipe( webp())
+            .pipe( dest('build/img'))
+}
+
+function versionAvif() {
+     return src('src/img/**/*.{png,jpg}')
+            .pipe( avif())
+            .pipe( dest('build/img'))
+}
+
+function dev() {
+    watch('src/scss/**/*.scss', css);
+    watch('src/img/**/*', imagenes);
 }
 
 exports.css = css;
+exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.default = series( imagenes, versionWebp, versionAvif, css, dev )
+
+
+// Series - Ejecuta una tarea y asta que termine ejecuta la otra tarea.
+// Parallel - Ejecuta todas las tareas a la vez.
--- readme.md
+++ readme.md
@@ -6,3 +6,17 @@
 ## PASO DOS
      1. npm i --save-dev gulp
      2. npm i --save-dev sass gulp-sass 
+## PASO DOS
+     1. npm i --save-dev autoprefixer gulp-postcss 
+
+
+### PREGUNTA DE ENTREVISTA DE SASS
+
+> Para que este archivo utiliza el guio abajo _header.scss (es por que se va incluir en otro archivo )
+
+
+## DEPENDENCIA DE IMAGENES PARA HACERLAS MAS LIVIANAS
+
+> npm install --save-dev gulp-imagemin@7.1.0
+
+> npm i --save-dev gulp-webp
